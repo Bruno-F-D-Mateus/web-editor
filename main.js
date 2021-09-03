f51af4c5b2025cdf7bc8e16bb5c668bbfde0df42
Bruno Mateus <brunofortunatomateus@gmail.com>
2021-09-03 16:04:20 +0100

    Editando o Título, Fechamento e abertura do app

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
-async function createWindow(){
+async function createWindow() {
     mainWindow = new BrowserWindow({
         widht: 800,
-        height: 500
+        height: 500,
+        title: 'Editor de Texto',
+        webPreferences: {
+            contextIsolation: true
+        }
     });
-    
     await mainWindow.loadFile("src/pages/editor/index.html");
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate',function(){
+    if(BrowserWindow.getAllWindows().length === 0) createWindow();
+});
